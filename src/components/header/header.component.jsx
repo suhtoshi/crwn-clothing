2400ace937a683354545dac2c172d465b6c4a20e
suhtoshi <haydenk.lewis@gmail.com>
2021-06-17 16:31:31 -0500

    added sign in on header and adjusted padding on signin page

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -13,8 +13,11 @@ const Header = () => (
             <Link className='option' to='/shop'>
                 SHOP
             </Link>
-            <Link className='contact' to='/shop'>
+            <Link className='option' to='/shop'>
                 CONTACT
+            </Link>
+            <Link className='option' to='/signin'>
+                SIGN IN
             </Link>
         </div>
     
