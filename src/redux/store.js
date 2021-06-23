5c4db930840fd91a4b032e78c783906197bc2c52
suhtoshi <haydenk.lewis@gmail.com>
2021-06-23 13:14:40 -0500

    created collection pages, created shop data,reducer, and selectors to generate pages for each collection

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from 'redux-logger';
+import { persistStore } from "redux-persist";
+
 
 
 import rootReducer from "./root-reducer";
@@ -8,4 +10,7 @@ const middlewares = [logger];
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+const persistor = persistStore(store);
+
+export {store, persistor};
+
